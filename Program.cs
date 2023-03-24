3186170ab0c761b3180122bea7830caed0168310
Nikita1600 <125687366+Nikita1600@users.noreply.github.com>
2023-03-24 20:30:20 +0530

    greeting msg

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace day13genericprogram
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Generic Program");
+            Console.WriteLine("Please Enter the FirstValue:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the SecondValue:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter the ThirdValue:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int val = FindMax.MaximumNum(num1, num2, num3);
+            Console.WriteLine("{0} is greater", val);
+        }
+
+    }
+}
